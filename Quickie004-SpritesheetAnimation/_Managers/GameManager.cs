1cab7cc9248a3bf8e9e6a79f3a5b1abc23c7877f
kcoul <kieran.coulter@gmail.com>
2025-01-03 18:45:21 -0800

    replace coin with pigeon

--- Quickie004-SpritesheetAnimation/_Managers/GameManager.cs
+++ Quickie004-SpritesheetAnimation/_Managers/GameManager.cs
@@ -2,25 +2,29 @@ namespace Quickie004;
 
 public class GameManager
 {
-    private Coin _coin;
+    //private Coin _coin;
+    private Pigeon1 _pigeon1;
     private Hero _hero;
 
     public void Init()
     {
-        _coin = new(new(300, 300));
+        //_coin = new(new(300, 300));
+        _pigeon1 = new(new(300, 300));
         _hero = new();
     }
 
     public void Update()
     {
         InputManager.Update();
-        _coin.Update();
+        //_coin.Update();
+        _pigeon1.Update();
         _hero.Update();
     }
 
     public void Draw()
     {
-        _coin.Draw();
+        //_coin.Draw();
+        _pigeon1.Draw();
         _hero.Draw();
     }
 }
--- Quickie004-SpritesheetAnimation/_Models/Pigeon1.cs
+++ Quickie004-SpritesheetAnimation/_Models/Pigeon1.cs
@@ -1,12 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Quickie004._Models
+namespace Quickie004;
+
+public class Pigeon1
 {
-    internal class Pigeon1
+    private static Texture2D _texture;
+    private Vector2 _position;
+    private readonly Animation _anim;
+
+    public Pigeon1(Vector2 pos)
     {
+        _texture ??= Globals.Content.Load<Texture2D>("pigeon_walking-Sheet");
+        _anim = new(_texture, 4, 1, 0.1f);
+        _position = pos;
+    }
+
+    public void Update()
+    {
+        _anim.Update();
+    }
+
+    public void Draw()
+    {
+        _anim.Draw(_position);
     }
 }
